0eb42395afd8fcef4b95c901271ce43eb6b9fc9e
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-05-30 23:04:35 +0200

    test(resp): ajoute un test pour l'encodage de strings

--- tests/Shared.UnitTests/Resp.cs
+++ tests/Shared.UnitTests/Resp.cs
@@ -20,4 +20,22 @@ public class RespTests
 
     Assert.Equal(expected, result.ToString());
   }
+
+  [Theory]
+  [InlineData("+OK\r\n")]
+  [InlineData("-NOT OK\r\n")]
+  [InlineData("$5\r\nHELLO\r\n")]
+  [InlineData("$6\r\n\nWORLD\r\n")]
+  [InlineData("$0\r\n\r\n")]
+  public void StringEncodeDecode(string input)
+  {
+
+    using var stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+    using var reader = new StreamReader(stream);
+
+    var item = Item.Decode(reader);
+    var encoded = item.Encode();
+
+    Assert.Equal(input, encoded);
+  }
 }
