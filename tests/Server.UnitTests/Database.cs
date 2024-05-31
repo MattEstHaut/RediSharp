ca4b7ce62cd96657a0d495f86b80a2b2df4ee527
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-05-31 17:46:54 +0200

    test(database): ajoute des tests unitaires

---
+++ tests/Server.UnitTests/Database.cs
@@ -0,0 +1,53 @@
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Server.UnitTests;
+
+public class DatabaseTests
+{
+  [Theory]
+  [InlineData("key", "value")]
+  [InlineData("", "value")]
+  [InlineData("key", "")]
+  [InlineData("", "")]
+  public void SetGet(string key, string value)
+  {
+    var db = new Database();
+    db.Set(key, value);
+    var result = db.Get(key);
+    Assert.Equal(value, result);
+  }
+
+  [Theory]
+  [InlineData("key1", "value", 50)]
+  [InlineData("key2", "value", 10)]
+  public void SetGetEx(string key, string value, long ex)
+  {
+    var db = new Database();
+    db.Set(key, value, ex);
+    var result = db.Get(key);
+    Assert.Equal(value, result);
+
+    Thread.Sleep((int)ex + 10);
+
+    result = db.Get(key);
+    Assert.Null(result);
+  }
+
+  [Fact]
+  public void AutoClean()
+  {
+    var db = new Database(10);
+    db.Set("key1", "value");
+    db.Set("key2", "value", 50);
+    db.Set("key3", "value", 20);
+
+    Thread.Sleep(100);
+
+    var data = db.GetType()
+      .GetField("_data", BindingFlags.NonPublic | BindingFlags.Instance)?
+      .GetValue(db) as ConcurrentDictionary<string, string>;
+
+    Assert.Equal(1, data?.Count);
+  }
+}
