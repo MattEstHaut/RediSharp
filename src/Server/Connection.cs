3d414b062b96ba79075aadf1e76f314a0f152606
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-05-31 19:31:08 +0200

    style(server): formatage du code

--- src/Server/Connection.cs
+++ src/Server/Connection.cs
@@ -4,41 +4,41 @@ using Shared.Resp;
 
 public class ConnectionHandler
 {
-  private readonly CommandManager _manager;
+    private readonly CommandManager _manager;
 
-  public ConnectionHandler(CommandManager manager)
-  {
-    _manager = manager;
-  }
-
-  public void Handle(TcpClient socket)
-  {
-    using var client = new Client(socket);
-
-    try
+    public ConnectionHandler(CommandManager manager)
     {
-      while (socket.Connected)
-      {
-        var request = client.Read();
-
-        if (request is not ItemArray array)
-          throw new Exception("Expected array");
-
-        var args = array.Items.ConvertAll(i => i.ToString() ?? "").ToArray();
-
-        if (args.Length == 0)
-          throw new Exception("Expected at least 1 argument");
-
-        var command = args[0];
-        var response = _manager.Execute(command, args[1..]);
-        client.Send(response);
-      }
+        _manager = manager;
     }
-    finally { }
-  }
 
-  public async Task HandleAsync(TcpClient socket)
-  {
-    await Task.Run(() => Handle(socket));
-  }
+    public void Handle(TcpClient socket)
+    {
+        using var client = new Client(socket);
+
+        try
+        {
+            while (socket.Connected)
+            {
+                var request = client.Read();
+
+                if (request is not ItemArray array)
+                    throw new Exception("Expected array");
+
+                var args = array.Items.ConvertAll(i => i.ToString() ?? "").ToArray();
+
+                if (args.Length == 0)
+                    throw new Exception("Expected at least 1 argument");
+
+                var command = args[0];
+                var response = _manager.Execute(command, args[1..]);
+                client.Send(response);
+            }
+        }
+        finally { }
+    }
+
+    public async Task HandleAsync(TcpClient socket)
+    {
+        await Task.Run(() => Handle(socket));
+    }
 }
--- src/Server/Server.cs
+++ src/Server/Server.cs
@@ -3,34 +3,34 @@ using System.Net.Sockets;
 
 public class Server
 {
-  private readonly TcpListener _listener;
-  private readonly ConnectionHandler _handler;
-  private readonly CommandManager _manager;
+    private readonly TcpListener _listener;
+    private readonly ConnectionHandler _handler;
+    private readonly CommandManager _manager;
 
-  public Server(int port, Database db)
-  {
-    _listener = new TcpListener(IPAddress.Any, port);
-    _manager = new CommandManager(db);
-    _handler = new ConnectionHandler(_manager);
-  }
-
-  public void Run()
-  {
-    _listener.Start();
-    _ = _manager.StartAsync();
-
-    while (_listener.Server.IsBound)
+    public Server(int port, Database db)
     {
-      var socket = _listener.AcceptTcpClient();
-      _ = _handler.HandleAsync(socket);
+        _listener = new TcpListener(IPAddress.Any, port);
+        _manager = new CommandManager(db);
+        _handler = new ConnectionHandler(_manager);
     }
-  }
 
-  public void Stop()
-  {
-    _listener.Stop();
-    _manager.Stop();
-  }
+    public void Run()
+    {
+        _listener.Start();
+        _ = _manager.StartAsync();
 
-  public void RunAsync() => Task.Run(() => Run());
+        while (_listener.Server.IsBound)
+        {
+            var socket = _listener.AcceptTcpClient();
+            _ = _handler.HandleAsync(socket);
+        }
+    }
+
+    public void Stop()
+    {
+        _listener.Stop();
+        _manager.Stop();
+    }
+
+    public void RunAsync() => Task.Run(() => Run());
 }
