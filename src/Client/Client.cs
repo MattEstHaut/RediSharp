71fa0a50807ebf4088268130af48394c3a5d0111
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-05-31 20:17:20 +0200

    style(client): formatage du code

--- src/Client/Client.cs
+++ src/Client/Client.cs
@@ -3,17 +3,17 @@ using Shared.Comm;
 
 public class Client
 {
-  private readonly Server _server;
+    private readonly Server _server;
 
-  public Client(TcpClient socket) => _server = new Server(socket);
+    public Client(TcpClient socket) => _server = new Server(socket);
 
-  public void Run()
-  {
-    while (CLI.Read(out var command))
+    public void Run()
     {
-      var tokens = CLI.Tokenize(command);
-      var response = _server.Send(tokens);
-      CLI.Write(response);
+        while (CLI.Read(out var command))
+        {
+            var tokens = CLI.Tokenize(command);
+            var response = _server.Send(tokens);
+            CLI.Write(response);
+        }
     }
-  }
 }
--- tests/Client.UnitTests/Cli.cs
+++ tests/Client.UnitTests/Cli.cs
@@ -1,19 +1,19 @@
 namespace Client.UnitTests
 {
-  public class CliTests
-  {
-    [Theory]
-    [InlineData("")]
-    [InlineData("GET", "GET")]
-    [InlineData("GET key", "GET", "key")]
-    [InlineData("SET key value", "SET", "key", "value")]
-    [InlineData("SET key \"value with spaces\"", "SET", "key", "value with spaces")]
-    [InlineData("SET key 'value with spaces'", "SET", "key", "value with spaces")]
-    [InlineData("SET key 'value with \\'quotes\\''", "SET", "key", "value with 'quotes'")]
-    public void Tokenize(string command, params string[] expected)
+    public class CliTests
     {
-      var tokens = CLI.Tokenize(command);
-      Assert.Equal(expected, tokens);
+        [Theory]
+        [InlineData("")]
+        [InlineData("GET", "GET")]
+        [InlineData("GET key", "GET", "key")]
+        [InlineData("SET key value", "SET", "key", "value")]
+        [InlineData("SET key \"value with spaces\"", "SET", "key", "value with spaces")]
+        [InlineData("SET key 'value with spaces'", "SET", "key", "value with spaces")]
+        [InlineData("SET key 'value with \\'quotes\\''", "SET", "key", "value with 'quotes'")]
+        public void Tokenize(string command, params string[] expected)
+        {
+            var tokens = CLI.Tokenize(command);
+            Assert.Equal(expected, tokens);
+        }
     }
-  }
 }
