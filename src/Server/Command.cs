085f4522ef4ff247858d5b3caebcfed73e2bb235
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-05-31 20:48:35 +0200

    feat(command): ajoute la commande SET

--- src/Server/Command.cs
+++ src/Server/Command.cs
@@ -66,3 +66,27 @@ public class UnknownCommand : Command
         return new SimpleError("Unknown command");
     }
 }
+
+public class SetCommand : Command
+{
+    public SetCommand(Database db) : base(db) { }
+
+    public override Item execute(params string[] args)
+    {
+        switch (args.Length)
+        {
+            case 2:
+                _db.Set(args[0], args[1]);
+                return new SimpleString("OK");
+            case 4:
+                if (args[2].ToUpper() != "EX")
+                    return new SimpleError("Expected EX keyword");
+                if (!int.TryParse(args[3], out int ms))
+                    return new SimpleError("Expected integer after EX keyword");
+                _db.Set(args[0], args[1], ms);
+                return new SimpleString("OK");
+            default:
+                return new SimpleError("Expected 2 or 4 arguments");
+        }
+    }
+}
