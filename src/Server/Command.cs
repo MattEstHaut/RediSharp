6327193fece065abc4beefceebaf803348a4d774
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-06-06 17:30:00 +0200

    feat(server): ajoute la commande TAIL

--- src/Server/Command.cs
+++ src/Server/Command.cs
@@ -24,6 +24,7 @@ public abstract class Command
             "TTL" => new TTLCommand(db),
             "APPEND" => new AppendCommand(db),
             "POP" => new PopCommand(db),
+            "TAIL" => new TailCommand(db),
             _ => new UnknownCommand(db),
         };
     }
@@ -222,6 +223,39 @@ public class PopCommand : Command
 
         var result = value.Substring(0, count);
         value = value.Substring(count);
+        _db.SetValue(args[0], value);
+        _db.Unlock();
+
+        return new BulkString(result);
+    }
+}
+
+public class TailCommand : Command
+{
+    public TailCommand(Database db) : base(db) { }
+
+    public override Item execute(params string[] args)
+    {
+        if (args.Length != 2)
+            return new SimpleError("Expected 2 argument");
+
+        if (!int.TryParse(args[1], out int count))
+            return new SimpleError("Expected integer as second argument");
+
+        if (count < 0)
+            return new SimpleError("Expected non-negative integer as second argument");
+
+        _db.Lock();
+        if (_db.Get(args[0]) is not string value)
+        {
+            _db.Unlock();
+            return new Null();
+        }
+
+        if (count >= value.Length) count = value.Length;
+
+        var result = value.Substring(value.Length - count);
+        value = value.Substring(0, value.Length - count);
         _db.SetValue(args[0], value);
         _db.Unlock();
 
